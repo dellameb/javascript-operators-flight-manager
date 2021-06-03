cd5d1e8874147bd93e3302fa10f7edd606178cdb
Debra Mella <debra.mella@libertymutual.com>
2021-06-03 12:31:40 -0500

    changes from exercise 2.1

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,16 @@
 function Flights() {
 
-}
 
+  function calculateNumberOfFlights(passengers, capacity) {
+
+    if (passengers % capacity === 0) {
+      nbrFlights = passengers / capacity;
+    }
+    else {
+      nbrFlights =  Math.floor(passengers / capacity) + 1;
+    }
+    return nbrFlights;
+  }
+  return {calculateNumberOfFlights};
+}
+module.exports = Flights();
